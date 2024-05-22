0e31599c5da47a14f2293066f4148296a0ddea09
Gabrijel-Cobanov <gabrijelcobanov@gmail.com>
2024-05-22 16:50:55 +0200

    Napravio header, dodao sliku i dummy text, smišljam upečatljivo i marketabilno ime
    Trebam napraviti responzivnost za širinu ekrana da se prilagodi kako spada

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,11 @@
 import * as React from "react";
+import "../ComponentsCSS/Header.css"
  
 
 export default function Header() {
     return (
-        <p id="header">THIS IS SOME TEXT</p>
+        <div id="header">
+            <img src="Images/logo_img.jpg" alt="logo" id="logo_image"></img>
+            <h1 id="app_name"> Tkom Package Delivery Manager </h1>
+        </div>
     );}
